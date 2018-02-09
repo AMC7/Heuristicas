6e0e6f1c24c18cd920dbc1ed66dbb291e230b543
AMC7 <antoniomartinezcruz@ciencias.unam.mx>
2018-02-08 20:13:18 -0600

    Agregue la estructura de ant

---
+++ src/main/principal/Principal.java
@@ -0,0 +1,17 @@
+package principal;
+import static util.Print.*;
+import static util.ManejadorDeArchivos.*;
+import static util.MyString.*;
+import util.Dupla;
+import util.Punto;
+import java.util.LinkedList;
+import java.lang.Math;
+import java.util.Hashtable;
+/**@version 1.0
+   @author Antonio Martinez Cruz*/
+public class Principal{
+	
+	public static void main(String [] args){
+		Main main= new Main();
+	}
+}
